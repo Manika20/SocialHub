378a77cf3bf80c15ce57943604b9189a161fef23
manika <goyalmanika20@gmail.com>
2024-04-16 21:04:29 +0530

    changes in mongo file

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const dbUri = process.env.SOCIALHUB_DB; // Fetch URI from environment variables
-    await mongoose.connect(dbUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(dbUri);
     console.log("Connected to MongoDB!");
   } catch (err) {
     console.log("Connection to MongoDB failed", err);
