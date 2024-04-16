604750381119e724af7722215b3acb20ac34f6b6
manika <goyalmanika20@gmail.com>
2024-04-16 19:26:41 +0530

    changes in mongo file

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,9 @@ const env = require("./environment");
 console.log(env.db);
 const connectDB = async () => {
   try {
-    await mongoose.connect(env.db);
+    await mongoose.connect(
+      "mongodb+srv://goyalmanika20:L6ZKwDY4DaV9LxQU@socialhub.0ydpadf.mongodb.net/?retryWrites=true&w=majority&appName=socialhub"
+    );
     console.log("connected to mongoo db!");
   } catch (err) {
     console.log("connect failed", err);
