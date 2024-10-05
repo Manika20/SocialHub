92dc34b9498fe87b17eb945a9d710790ffbeee91
manika <goyalmanika20@gmail.com>
2024-10-05 22:23:43 +0530

    ready to deploy

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const forgetPassword = require("../controllers/auth_controller");
-router.get("/", forgetPassword.open);
-router.post("/verify-email", forgetPassword.find);
-router.get("/reset-password", forgetPassword.newPassword);
-router.post("/reset", forgetPassword.reset);
-module.exports = router;
--- routes/likes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const postLikeController = require("../controllers/likes_controller");
-
-router.post("/toggle", postLikeController.toggleLike);
-module.exports = router;
